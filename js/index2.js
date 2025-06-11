926510d4a811fb876ac2429dd721d19320f21726
juanmherrero <105325161+juanmherrero@users.noreply.github.com>
2025-06-11 13:35:22 -0300

    carga inicial

---
+++ js/index2.js
@@ -0,0 +1,20 @@
+const carrito = document.getElementById('carrito');
+const prods = document.getElementById('listaProds');
+const listaProds = document.querySelector('#lista-carrito tbody')
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+
+
+
+// JavaScript para el efecto de scroll
+        const navbar = document.getElementById('navbar');
+        const logo = document.getElementById('logo');
+
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 50) {
+                navbar.classList.add('shrink');
+                logo.src = 'img/logo.png'; // Cambia al nuevo logo
+            } else {
+                navbar.classList.remove('shrink');
+                logo.src = 'img/log.png'; // Vuelve al logo original
+            }
+        });
